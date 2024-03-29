7d8145e2cc30c420cb676a72157527a8ec912be6
rocket310 <gomezacunav@gmail.com>
2024-03-29 01:54:47 -0600

    crear proyectos

--- app/nuevo-proyecto/page.tsx
+++ app/nuevo-proyecto/page.tsx
@@ -1,15 +1,105 @@
+'use client'
 import React from 'react'
 import SideNav from '../components/SideNav'
+import { useState } from 'react'
+
+export async function Crear(valores: any) {
+    //const valoresJSON = JSON.stringify(valores);
+    //console.log(valoresJSON);
+
+    fetch("http://localhost:9000/api/proyectos", {
+        method: 'POST',
+        body: JSON.stringify(valores),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        //window.location.reload()
+    })
+    .catch(error => console.error('Error:', error))
+    //alert("Proyecto creado con exito")
+    
+}
 
 const page = () => {
-    return (
-        <div className="flex h-screen bg-white">
-            <SideNav />
-            <div className="flex-1 px-4 pt-2 text-black">
-                Crear Nuevo Proyecto 
-            </div >
-        </div>
-    )
-}
+    const hoy = new Date(Date.now()).toISOString().split('T')[0];
+
+    const [valores, setValores] = useState(
+        {
+            nombre: '',
+            recursosN: '',
+            presupuesto: 0,
+            correoColaboradores: '',
+            estado: 'Pendiente',
+            descripcion: '',
+            fechaInicio: hoy,
+            fechaFin: '',
+            historialCambios: [],
+            correoResponsable: '',
+            tareas: []
+
+
+
+        }
+    );
+
+    const Form = () => {
+        const [valores, setValores] = useState({
+            nombre: '',
+            recursos: '',
+            presupuesto: '',
+            colaboradores: ''
+        });
+    };
+
+    const handleForm = (event: any) => {
+        event.preventDefault();
+        console.log(valores);
+        
+        
+        Crear(valores);
+    };
+
+    const handleInputChange = (event: any) => {
+        const { name, value } = event.target;
+        setValores({
+            ...valores,
+            [name]: value,
+        });
+    }
+
+    
+        return (
+            <div className="flex h-screen bg-white">
+                <SideNav />
+                <div className="flex-1 px-4 pt-2 text-black bg-green-100">
+                    <div className='card bg-gray-400 bg-opacity-50 px-10 py-8 mx-52 mt-11 shadow-xl'>
+                        <h1 className='text-xl font-bold'>Nuevo Proyecto</h1>
+                        <hr />
+                        <form  onSubmit={handleForm} className='card bg-gray-400 bg-opacity-50 px-10 py-8 mx-52 mt-11 shadow-xl'>
+                        <h5 className='mx-2 mt-2'>Nombre del Projecto</h5>
+                        <input type="text" placeholder="Type here" className="input input-bordered mt-2 bg-white mb-1" 
+                        name="nombre" value={valores.nombre} onChange={handleInputChange} required />
+                        <h5 className='mx-2 mt-2'>Recursos</h5>
+                        <input type="text" placeholder="Type here" className="input input-bordered mt-2 bg-white mb-1" 
+                        name="recursosN" value={valores.recursosN} onChange={handleInputChange} required/>
+                        <h5 className='mx-2 mt-2'>Presupuesto</h5>
+                        <input type="text" placeholder="Type here" className="input input-bordered mt-2 bg-white mb-1" 
+                        name="presupuesto" value={valores.presupuesto} onChange={handleInputChange} required/>
+                        <h5 className='mx-2 mt-2'>Colaboradores</h5>
+                        <input type="text" placeholder="Type here" className="input input-bordered mt-2 bg-white mb-1" 
+                        name="correoColaboradores" value={valores.correoColaboradores} onChange={handleInputChange} required/>
+                        <button type="submit" className="btn btn-defult mt-8 mr-80 shadow-lg">Crear</button>
+                        </form>
+                    </div>
+                </div >
+            </div>
+        )
+    }
+    
 
 export default page
