be2e8367d290b9e3ebcb86a99b7df928f30c7dcc
EsRoHi26 <108766267+EsRoHi26@users.noreply.github.com>
2024-03-31 14:47:28 -0600

    Informe General Front2.0

--- app/components/DdownPM.tsx
+++ app/components/DdownPM.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import SideNav from '../components/SideNav'
 import Link from 'next/link'
 
-import { Proyectos } from '../interfaces/users.interface'
+import { Proyectos } from '../interfaces/project.interface'
 import { Usuario2 } from '../interfaces/project.interface'
 
 
