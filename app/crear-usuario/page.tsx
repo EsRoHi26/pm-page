d0cd9ab3dbc79315180c9e426f072a2ff0ac6c89
EsRoHi26 <108766267+EsRoHi26@users.noreply.github.com>
2024-03-31 20:10:13 -0600

    test2

--- app/crear-usuario/page.tsx
+++ app/crear-usuario/page.tsx
@@ -7,7 +7,16 @@ const page = () => {
 
     return (
         <div className="flex h-screen bg-white">
-            <SideNav />
+            <div className="w-40 bg-gray-800 text-white xs:w-1/6">
+            <h1 className="text-2xl font-bold p-5">VISION</h1>
+            <div className=' bg-gray-600 px-5'>
+                <Link href={''}>
+                    <ul className="mt-5 space-y-2">
+                        <li>Login</li>
+                    </ul>
+                </Link>
+            </div>
+        </div>
             <DdownP/>
         </div>
     )
