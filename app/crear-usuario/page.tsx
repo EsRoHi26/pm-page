dfb253756c0e64a8fab8a524322bbfb7a11db0ad
EsRoHi26 <108766267+EsRoHi26@users.noreply.github.com>
2024-03-31 20:13:52 -0600

    t

--- app/crear-usuario/page.tsx
+++ app/crear-usuario/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
             <div className="w-40 bg-gray-800 text-white xs:w-1/6">
             <h1 className="text-2xl font-bold p-5">VISION</h1>
             <div className=' bg-gray-600 px-5'>
-                <Link href={''}>
+                <Link href={'/'}>
                     <ul className="mt-5 space-y-2">
                         <li>Login</li>
                     </ul>
